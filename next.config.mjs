7f597e5360de3e7aca2d6c97d532c79b1be8aa70
kanglocal <develocal25@gmail.com>
2024-05-16 22:18:50 +0900

    chore: 프로젝트 생성

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,31 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin';
+import withPWA from 'next-pwa';
+import withPlugins from 'next-compose-plugins';
 
-export default nextConfig;
+const withVanillaExtract = createVanillaExtractPlugin();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+    return config;
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*',
+      },
+    ],
+  },
+};
+
+export default withPlugins(
+  [
+    [withPWA, {pwa:{dest:'public'}}]
+  ],
+  withVanillaExtract(nextConfig)
+)
